62a3947627545c9fba1bd716935e7ad80de7e454
Maxime Rochkoulets <mrochkoulets@gmail.com>
2026-07-23 12:15:13 +0200

    doc + typing of splines

--- decoupling/_splines/splines.py
+++ decoupling/_splines/splines.py
@@ -1,12 +1,20 @@
 import bsplx
-import jax.numpy as jnp
+import jax, jax.numpy as jnp
+from jaxtyping import jaxtyped, Array, Float, Int
+from beartype import beartype
 
-def design_matrix(z, knots, degree: int):
-    matrix = bsplx.design_matrix(z, knots, degree)
+@jax.jit(static_argnames='degree')
+@jaxtyped(typechecker=beartype)
+def design_matrix(inputs: Float[Array, 'n'], knots: Float[Array, 'k'], degree: int) -> Float[Array, 'n m']: # m = k - degree
+    ''' get the b-spline design matrix with an added column of ones '''
+    matrix = bsplx.design_matrix(inputs, knots, degree)
     matrix = jnp.concatenate([jnp.ones((matrix.shape[0], 1)), matrix], axis=1)
     return matrix
 
-def design_dmatrix(z, knots, degree: int):
-    dmatrix = bsplx.design_dmatrix(z, knots, degree)
+@jax.jit(static_argnames='degree')
+@jaxtyped(typechecker=beartype)
+def design_dmatrix(inputs: Float[Array, 'n'], knots: Float[Array, 'k'], degree: int) -> Float[Array, 'n m']: # m = k - degree
+    ''' get the b-spline derivative design matrix with an added column of zeros '''
+    dmatrix = bsplx.design_dmatrix(inputs, knots, degree)
     dmatrix = jnp.concatenate([jnp.zeros((dmatrix.shape[0], 1)), dmatrix], axis=1)
     return dmatrix
